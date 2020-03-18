51487b0637d8e53cc772b7c4736f1c2673e64592
radinmarinov <radinmarinov2021@u.northwestern.edu>
2020-03-18 13:02:21 -0500

    article summarizer

--- research_finder.py
+++ research_finder.py
@@ -10,6 +10,11 @@ import json
 import glob
 import re
 import requests
+import nltk
+from nltk.cluster.util import cosine_distance
+from nltk.corpus import stopwords
+import numpy as np
+import networkx as nx
 
 class ResearchFinder():
     """
@@ -132,24 +137,65 @@ class ResearchFinder():
             self.citation_dict = citation_dict
         return self.citation_dict
     
-    def summarize_paper(self, paper):
-        api_key = ''
-        url = 'https://api.smmry.com'
-        data = self.get_data()
-        text = data.loc[data['paper_id'] == paper, 'init_body_text'][0]
-        PARAMS = {
-                 'SM_API_KEY': api_key,
-                 'sm_api_input':text,
-                 'SM_URL': url}
-        r = requests.get(url = url, params = PARAMS) 
-        r.close()
-        request_data = r.json()
-        return request_data
+    def summarize_paper(self, paper_text, n_sentences):
+        
+        tokens = nltk.tokenize.sent_tokenize(paper_text)
+        sentences = []
+        for sentence in tokens:
+            sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+        stop_words = stopwords.words("english")
+        summarize_text = []
+        # Step 2 - Generate Similary Martix across sentences
+        sentence_similarity_martix = self.build_similarity_matrix(sentences, stop_words)
+        # Step 3 - Rank sentences in similarity martix
+        sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+        scores = nx.pagerank(sentence_similarity_graph)
+        # Step 4 - Sort the rank and pick top sentences
+        ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+        #print("Indexes of top ranked_sentence order are ", ranked_sentence)
+        for i in range(0, n_sentences):
+            summarize_text.append(" ".join(ranked_sentence[i][1]))
+        # Step 5 - Offcourse, output the summarize texr
+        #print("Summarize Text: \n", ". ".join(summarize_text))
+        return "\n".join(summarize_text)
     
+    def sentence_similarity(self, sent1, sent2, stopwords_=None):    
+        if stopwords_ is None:        
+            stopwords_ = []     
+        sent1 = [w.lower() for w in sent1]    
+        sent2 = [w.lower() for w in sent2]     
+        all_words = list(set(sent1 + sent2))    
+        vector1 = [0] * len(all_words)    
+        vector2 = [0] * len(all_words)     
+        # build the vector for the first sentence   
+        for w in sent1:       
+            if w in stopwords_:            
+                continue        
+            vector1[all_words.index(w)] += 1     
+        # build the vector for the second sentence    
+        for w in sent2:        
+            if w in stopwords_:           
+                continue        
+            vector2[all_words.index(w)] += 1     
+        
+        return 1 - cosine_distance(vector1, vector2)
     
+    def build_similarity_matrix(self, sentences, stop_words):
+        # Create an empty similarity matrix
+        similarity_matrix = np.zeros((len(sentences), len(sentences)))
+     
+        for idx1 in range(len(sentences)):
+            for idx2 in range(len(sentences)):
+                if idx1 == idx2: #ignore if both are same sentences
+                    continue 
+                similarity_matrix[idx1][idx2] = self.sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+        
+        return similarity_matrix
+
+
 if __name__ == '__main__':
     rf = ResearchFinder()
     data = rf.get_data()
     test_papers = rf.find_paper(['coronavirus', 'pregn'])
     citation_dict = rf.get_citation_dict()
-    summary = rf.summarize_paper(test_papers.paper_id[0])
+    summary = rf.summarize_paper(test_papers['init_body_text'][0], 7)
