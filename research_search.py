1cdaaf068ecdcdf491a55352496d76443a099fea
Nikhil Patel <nikhilpatel2021@u.northwestern.edu>
2020-03-21 20:26:43 -0500

    Summarys

--- research_search.py
+++ research_search.py
@@ -3,23 +3,65 @@ from research_finder import ResearchFinder
 
 app = Flask(__name__)
 
-
 rf = ResearchFinder()
 rf.load_data()
+SEARCH = False
+DATA = []
+ENTRY = ""
+SUMMARY = False
+SUMMARY_TEXT = ""
+PAPERS = []
 @app.route('/')
 def render():
-	return render_template("index.html", search = False, data = [], search_entry = "")
+	global SEARCH
+	global DATA
+	global ENTRY
+	global SUMMARY
+	global ARTICLE
+	global PAPERS
+	return render_template("index.html", search = SEARCH, data = DATA, search_entry = ENTRY, summary = SUMMARY, summary_text = SUMMARY_TEXT)
 
-@app.route('/', methods=['POST'])
+@app.route('/search', methods=['POST'])
 def search():
-	search = request.form['keywords']
-	keywords = search.split(" ")
-	papers = rf.find_paper(keywords)
-	formatted_data = []
+	global SEARCH
+	global DATA
+	global ENTRY
+	global SUMMARY
+	global ARTICLE
+	global PAPERS
+	SUMMARY = False
+	SEARCH = True
+	ENTRY = request.form['keywords']
+	keywords = ENTRY.split(" ")
+	PAPERS = rf.find_paper(keywords)
+	DATA = []
 	for x in range(0,5):
-		formatted_data.append([papers["init_title"].iloc[x], papers["init_abstract"].iloc[x][0:500] + "..."])
+		DATA.append([PAPERS["init_title"].iloc[x], "Abstract: " + PAPERS["init_abstract"].iloc[x][0:500] + "..."])
 
 	#print(formatted_data)
+	return render_template("index.html", search = SEARCH, data = DATA, search_entry = ENTRY, summary = SUMMARY, summary_text = SUMMARY_TEXT)
 
 
-	return render_template("index.html", search = True, data = formatted_data, search_entry = search)
+@app.route('/summary', methods=['POST'])
+def summary():
+	global SEARCH
+	global DATA
+	global ENTRY
+	global SUMMARY
+	global ARTICLE
+	global PAPERS
+	SEARCH = True
+	SUMMARY = True
+	ARTICLE = request.form['link']
+	SUMMARY_TEXT = rf.summarize_paper(PAPERS['init_body_text'].iloc[int(ARTICLE)], 7)
+	return render_template("index.html", search = SEARCH, data = DATA, search_entry = ENTRY, summary = SUMMARY, summary_text = SUMMARY_TEXT)
+	# keywords = search.split(" ")
+	# papers = rf.find_paper(keywords)
+	# formatted_data = []
+	# for x in range(0,5):
+	# 	formatted_data.append([papers["init_title"].iloc[x], "Abstract: " + papers["init_abstract"].iloc[x][0:500] + "..."])
+
+	# #print(formatted_data)
+
+
+	# return render_template("index.html", search = True, data = formatted_data, search_entry = search)
